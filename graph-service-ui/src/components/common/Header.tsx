4eabef1c7cdc3d821e0b3bdd8a261bbb1aa859aa
ShababKarim <skarim222@gmail.com>
2023-09-19 04:10:35 -0500

    mod: add Dockerfile for graph-service

--- graph-service-ui/src/components/common/Header.tsx
+++ graph-service-ui/src/components/common/Header.tsx
@@ -1,11 +1,5 @@
-import Link from "next/link";
-
 const Header = () => {
     return (
-        // <div className='container mx-auto text-center mb-6'>
-        //     <p className='text-2xl font-bold'><Link href='/'>EDA Graph</Link></p>
-        //     <p className='text-base font-light'>A service to trace inter-app communication through events</p>
-        // </div>
         <div
             className="bg-white px-6 py-20 text-center text-neutral-800">
             <h1 className="mb-6 text-5xl font-bold">EDA Graph</h1>
